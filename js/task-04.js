63ea08f8eae2c5b29b59ec25bc2216f099a78db7
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-15 19:28:49 +0200

    ДЗ приклад 4.1.

---
+++ js/task-04.js
@@ -0,0 +1,4 @@
+const counterValue = document.querySelector(`value`);
+const decrementButton = document.querySelector(`[data-action="decrement"]`);
+const incrementButton = document.querySelector(`[data-action="increment"]`);
+let value = 0;
