ff75da680499051e56b7178dc0b9cc56fe137be3
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-15 20:21:22 +0200

    update DZ4.1

--- js/task-04.js
+++ js/task-04.js
@@ -2,3 +2,11 @@ const counterValue = document.querySelector(`value`);
 const decrementButton = document.querySelector(`[data-action="decrement"]`);
 const incrementButton = document.querySelector(`[data-action="increment"]`);
 let value = 0;
+decrementButton.addEventListener(`click`, () => {
+    value --;
+    counterValue.textContent = value;
+});
+incrementButton.addEventListener(`click`, () => {
+    value ++;
+    counterValue.textContent = value;
+});
