9f7ca005081232f8adbb517735ad93e9d4ee2098
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-16 13:25:59 +0200

    завершена ДЗ приклад 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,12 +1,19 @@
-const counterValue = document.getElementById(`value`);
+const counterValue = document.getElementById(`value`); 
+// Створив змінну counterValue, в якій буде зберігатися поточне значення лічильника.
+//  Змінна має атрибут`id="value"`
 const decrementButton = document.querySelector(`[data-action="decrement"]`);
 const incrementButton = document.querySelector(`[data-action="increment"]`);
+// Створив змінні які посилаються на елементи кнопок для зменьшення та збільшення значення лічильника відповідно.
+//  Кнопки мають атрибути data - action="decrement" и data - action="increment" відповідно.
 let value = 0;
+// створив змінну та ініціалізував її значенням до 0.
 decrementButton.addEventListener(`click`, () => {
     value --;
     counterValue.textContent = value;
 });
+// функція зменшує лічильник на одиницю та записує оновлене значення 'value'
 incrementButton.addEventListener(`click`, () => {
     value ++;
     counterValue.textContent = value;
 });
+// функція зменшує лічильник на одиницю та записує оновлене значення 'value'
