8c64d85ff667adeeffdf61aefaa2400a72f77f26
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-17 19:26:27 +0200

    ДЗ 6 приклад 5.2

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,13 @@
 const nameInput = document.getElementById("name-input");
+// Створив змінну nameInput
 const nameOutput = document.getElementById("name-output");
-
+// Створив змінну nameOutput
 nameInput.addEventListener("input", () => {
     if (nameInput.value === "") {
-    nameOutput.textContent = "Anonymous";
+        nameOutput.textContent = "Anonymous";
+      // Якщо поле введення порожнє, використовується значення "Anonymous".
     } else {
-    nameOutput.textContent = nameInput.value;
+        nameOutput.textContent = nameInput.value;
+      // Яке значення введено в поле таке і виводиться.
     }
 });
