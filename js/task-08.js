623e1fdb6557fc3d7bddf7f392372638aa433e74
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-18 21:03:25 +0200

    ДЗ 6 завдання 8.23

--- js/task-08.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector(`.login-form`);
-
-form.addEventListener(`submit`, onFormSubmit);
-
-function onFormSubmit(event) {
-    event.prevenDefault();
-    console.log(event.currentTarget);
-    console.log(`це форма сабміт`);
-}
