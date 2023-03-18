dde33db75b450960ff2b1a2f31dd1ac4a4d919b3
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-03-18 20:51:17 +0200

    ДЗ 6 завдання8.1 початок

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,9 @@
+const form = document.querySelector(`.login-form`);
 
+form.addEventListener(`submit`, onFormSubmit);
+
+function onFormSubmit(event) {
+    event.prevenDefault();
+    console.log(event.currentTarget);
+    console.log(`це форма сабміт`);
+}
